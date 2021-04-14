2602ca48d96a2119c9610932df983ef9c9718ce9
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-14 16:35:50 -0300

    Ex2 - Cria triangulo retangulo de asteriscos

---
+++ exercises/bloco-4/dia_4.2/introducao-a-javascript-e-logica-de-programacao_3/trianguloRetanguloDeAsteriscos.js
@@ -0,0 +1,9 @@
+//Exercício 2
+let n = 5;
+let symbol = '*';
+let imputSymbol = '';
+
+for (index = 0; index <= n; index += 1) {
+  console.log(imputSymbol);
+  imputSymbol = imputSymbol + symbol; 
+}
