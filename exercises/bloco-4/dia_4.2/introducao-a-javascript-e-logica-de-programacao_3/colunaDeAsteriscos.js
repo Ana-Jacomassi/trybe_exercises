d6753d940d105d40a46a13a3eb2a9d79b9133f52
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-14 16:26:46 -0300

    Ex1 - Cria coluna de astericos

---
+++ exercises/bloco-4/dia_4.2/introducao-a-javascript-e-logica-de-programacao_3/colunaDeAsteriscos.js
@@ -0,0 +1,11 @@
+// Exercício 1
+let n = 5;
+let symbol = '*';
+let imputSymbol = [];
+
+for (index = 0; index < n; index += 1) {
+  imputSymbol = imputSymbol + symbol; 
+};
+for (index = 0; index < n; index += 1) {
+  console.log(imputSymbol);
+}
