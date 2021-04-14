e2e5aa31b77556d3850f04aa530e4f916cc6c6fa
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-14 15:06:10 -0300

    Ex6 - verifica se há valores impares no array

---
+++ exercises/bloco-4/dia_4.2/exercises-introducao-a-javascript-e-logica-de-programacao_2/Exercicio6.js
@@ -0,0 +1,14 @@
+// Exercício
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let imparesNumb = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+      imparesNumb += 1;
+  }
+}
+if (imparesNumb === 0) {
+  console.log("Nenhum valor ímpar encontrado.");
+} else {
+  console.log(imparesNumb);
+}
