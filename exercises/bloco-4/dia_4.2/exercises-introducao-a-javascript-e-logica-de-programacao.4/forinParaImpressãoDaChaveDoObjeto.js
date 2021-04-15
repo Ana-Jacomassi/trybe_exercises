38803f5a184bbc0c3ca824f7a595d7bf1fe395ff
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-15 01:28:05 -0300

    Ex3 - imprime chave do objeto utilizando for in

--- exercises/bloco-4/dia_4.2/exercises-introducao-a-javascript-e-logica-de-programacao.4/forinParaImpressãoDaChaveDoObjeto.js
+++ exercises/bloco-4/dia_4.2/exercises-introducao-a-javascript-e-logica-de-programacao.4/forinParaImpressãoDaChaveDoObjeto.js
@@ -1,8 +1,11 @@
-//Exercício 1
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log('Bem vinda, ' + info.personagem + '!');
+info['recorrente'] = 'Sim'
+
+for (let key in info) {
+  console.log(key);  
+}
