408602f60555412a4eb5c5508e29f9b088e8d709
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-30 15:58:33 -0300

    fatorial

--- exercises/7-bloco/calculoFatorial.js
+++ exercises/7-bloco/calculoFatorial.js
@@ -2,14 +2,21 @@ const assert = require('assert');
 
 const fatorial = (number) => {
   let multiplier = 1
+  
+  if(typeof(number) !== 'number') {
+    throw new Error('Este campo precisa ser preenchido por algum número, engraçadinho(a) :p');
+  }
+  
   for(let i = number; i >= 1; i -= 1) {
    multiplier *= i;
   }
   return multiplier
 } 
-fatorial(3);
+fatorial('string');
 
 
 assert.strictEqual(typeof(fatorial), 'function');
 assert.strictEqual(fatorial(1), 1);
 assert.strictEqual(fatorial(3), 6);
+
+// exercício com testes de assert.
